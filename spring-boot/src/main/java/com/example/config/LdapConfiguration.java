312245ba09f6b455d1bd3ee75ccd3e92174f5f07
Bogdan Solga <bogdan.solga@gmail.com>
2015-03-04 07:05:42 +0200

    Moved the context source bean to the LDAP config, removed the LDAP server configurer

--- spring-boot/src/main/java/com/example/config/LdapConfiguration.java
+++ spring-boot/src/main/java/com/example/config/LdapConfiguration.java
@@ -1,32 +1,50 @@
 package com.example.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.ldap.repository.config.EnableLdapRepositories;
-import org.springframework.security.ldap.authentication.PasswordComparisonAuthenticator;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
 
 @Configuration
+@PropertySource("classpath:ldap.properties")
 @EnableLdapRepositories("com.example.repositories.ldap")
 public class LdapConfiguration {
 
-    @Autowired
-    private LdapServerConfigurer ldapServerConfigurer;
+    @Value("${ldap.url}")
+    private String ldapUrl;
+
+    @Value("${ldap.userDn}")
+    private String ldapManagerDn;
+
+    @Value("${ldap.password}")
+    private String ldapPassword;
+
+    @Bean
+    public LdapContextSource contextSource() {
+        DefaultSpringSecurityContextSource contextSource = new DefaultSpringSecurityContextSource(ldapUrl);
+
+        contextSource.setUserDn(ldapManagerDn);
+        contextSource.setPassword(ldapPassword);
+
+        return contextSource;
+    }
 
     @Bean
     public LdapTemplate ldapTemplate() {
         final LdapTemplate ldapTemplate = new LdapTemplate();
 
-        ldapTemplate.setContextSource(ldapServerConfigurer.contextSource());
+        ldapTemplate.setContextSource(contextSource());
 
         return ldapTemplate;
     }
 
     @Bean
     public FilterBasedLdapUserSearch filterBasedLdapUserSearch() {
-        return new FilterBasedLdapUserSearch("ou=accounts", "cn={0}", ldapServerConfigurer.contextSource());
+        return new FilterBasedLdapUserSearch("ou=accounts", "cn={0}", contextSource());
     }
 }
